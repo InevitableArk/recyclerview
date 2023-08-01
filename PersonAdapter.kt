3d24e21aba71ab75b4818f1a7f7d9aaacffafbc1
InevitableArk <71696601+InevitableArk@users.noreply.github.com>
2023-08-01 23:59:15 +0530

    Update and rename ContactsAdapter.kt to PersonAdapter.kt

--- PersonAdapter.kt
+++ PersonAdapter.kt
@@ -1,32 +1,30 @@
-package com.example.hello
+package com.example.words
 
-import android.app.Person
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.hello.databinding.ActivityMainBinding
-
+import com.example.words.databinding.ActivityMainBinding
 
 private lateinit var binding: ActivityMainBinding
 
-class ContactsAdapter(private val context:Context, private val contacts:List<Person>) : RecyclerView.Adapter<ContactsAdapter.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view: View = LayoutInflater.from(context).inflate(R.layout.item_person, parent, false)
-        return ViewHolder(view)
+class PersonAdapter(val context: Context, val person:List<Person>) : RecyclerView.Adapter<PersonAdapter.ViewHolder>() {
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view: View = LayoutInflater.from(context).inflate(R.layout.item, parent,false)
+        return ViewHolder(view)
     }
 
-    override fun getItemCount() = contacts.size
+    override fun getItemCount() = person.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val contact : Person = contacts[position]
+        val contact : Person = person[position]
         holder.bind(contact)
     }
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         val tvName = itemView.findViewById<TextView>(R.id.tvName)
         val tvAge = itemView.findViewById<TextView>(R.id.tvAge)
 
