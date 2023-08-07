2e5ce1146ad5e5fec7d88378a3d019536ec6a3df
InevitableArk <71696601+InevitableArk@users.noreply.github.com>
2023-08-07 16:39:30 +0530

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -4,10 +4,13 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.words.databinding.ActivityMainBinding
+import java.io.Serializable
 
-data class Person(val name:String,val age:Int)
+data class Person(val name:String,val age:Int):Serializable
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,7 +19,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate((layoutInflater))
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val person = createPerson()
         binding.rvPerson.adapter = PersonAdapter(this, person)
@@ -24,10 +27,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun createPerson():List<Person>{
-        for (i in 1..10) {
+        for (i in 1..100) {
             val person = Person("Person $i", i)
             contacts.add(person)
         }
         return contacts
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.miAdd){
+            val myIntent = Intent(this, AddPersonActivity::class.java)
+            startActivity(intent)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
